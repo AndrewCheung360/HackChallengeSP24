281262c500474602ce6ff3a8dc0b930f46c02b86
GabePasternack <gabe.pasternack@bounceimaging.com>
2024-05-02 22:12:28 -0400

    Hid access key

--- backend/src/s3_functions.py
+++ backend/src/s3_functions.py
@@ -21,8 +21,8 @@ def download_file(file_name, bucket, id):
   object_name = file_name
   s3_client = boto3.client(
     's3',
-    aws_access_key_id="AKIAZQ3DQI4NOZOGJVN6",
-    aws_secret_access_key="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"
+    aws_access_key_id=ACCESS_ID,
+    aws_secret_access_key=ACCESS_KEY
   )
   to_save = "downloads/" + str(id) + ".pdf"
   response = s3_client.download_file(bucket, object_name, to_save)
